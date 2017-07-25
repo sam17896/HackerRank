1e7c9f6278b618e0b49beae5e863f25d1799bc35
sam17896 <ahsan.kai@gmail.com>
2017-07-25 20:03:46 +0500

    Event Tree

---
+++ EvenTree/src/eventree/EvenTree.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventree;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sohail Arshad
+ */
+public class EvenTree {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int vertices = s.nextInt();
+        int edges = s.nextInt();
+        Node[] nodes = new Node[vertices];
+        while (vertices -- > 0)
+            nodes[vertices] = new Node();
+        while(edges -- > 0)
+            nodes[s.nextInt()-1]
+               .setParent(nodes[s.nextInt()-1]);
+        int disconnects = -1;
+        for(Node n:nodes)
+            if(n.descendantCount % 2 == 1)
+                disconnects++;
+        System.out.println(disconnects);
+    }
+}
+class Node{    
+    private Node parent;
+    int descendantCount = 0;
+    void setParent(Node parent){
+        this.parent = parent;
+        Node ancestor = parent;
+        while(ancestor != null){
+            ancestor.descendantCount ++;
+            ancestor = ancestor.getParent();
+        }
+    }
+    Node getParent(){
+        return this.parent;
+    }
+}
+
